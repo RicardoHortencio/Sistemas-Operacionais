612775023e1e545be09e21891c12d77ec127f746
LEONEXSTxD <leonardo.vasconcelos@hotmail.com.br>
2017-06-20 09:27:14 -0300

    Inclusão do main

---
+++ README.txt
@@ -0,0 +1,5 @@
+Passos para a compilação no sistema operacional windows:
+
+1- Realize o download do programa Dev-C++ IDE disponivel no link:https://sourceforge.net/projects/orwelldevcpp/
+2-Após instalar o programa va na aba file > open selecione o arquivo torta e pressione F11 para execultar 
+
--- torta.c
+++ torta.c
@@ -57,3 +57,42 @@ pthread_exit(NULL);
 }
 
 
+main( int argc, char *argv[ ], char *envp[ ] )
+
+{
+
+      int i,s,estado,Assar=4,comer=8;
+
+      pthread_mutex_init(&mutex_Torta, NULL);
+
+      pthread_create(&threads[0],NULL,Assou,(void *)&Assar);
+
+      pthread_create(&threads[1],NULL,comeu,(void *)&comer);
+
+      for(i=0; i<2; i++)
+
+      {          
+
+        s=pthread_join(threads[i], (void **) &estado);
+
+        if (s)
+
+        {
+
+            perror("Erro no join");
+
+            exit(-1);
+
+        }
+
+        printf("O thread %d terminou com o estado %d\n",i,estado);
+
+    }
+
+printf("A quantidade de torta é =  %d\n",Torta);
+
+      pthread_mutex_destroy(&mutex_Torta); 
+
+      pthread_exit(NULL);
+
+}
