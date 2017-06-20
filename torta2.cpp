679d649600798b15eb6016a9ba3f7abb832b3d4a
EdnilsonLopes <ednilson.b.l@gmail.com>
2017-06-20 09:24:38 -0300

    Método "Assou"
    Adicionei mais um método para o trabalho

---
+++ torta2.cpp
@@ -0,0 +1,59 @@
+
+
+
+#include <pthread.h>
+
+#include <stdio.h>
+
+
+
+int Torta=10;
+
+pthread_t threads[2]; 
+
+pthread_mutex_t mutex_Torta;
+
+void *comeu(void *quantidade)
+
+{
+
+      int *b= (int *) quantidade;
+
+
+pthread_mutex_lock(&mutex_Torta);
+
+Torta -= *b;
+
+
+pthread_mutex_unlock(&mutex_Torta);
+
+pthread_exit(NULL);
+
+     
+
+}
+
+
+
+
+void *Assou(void *quantidade)
+
+{
+
+      int *assado= (int *) quantidade;
+
+     
+
+pthread_mutex_lock(&mutex_Torta);
+
+Torta += *assado;
+
+
+
+pthread_mutex_unlock(&mutex_Torta);
+
+pthread_exit(NULL);
+
+}
+
+
